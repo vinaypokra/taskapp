fdba6267ad7908c82d72d5e840ef7805dd19616b
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-03-10 01:38:46 +0530

    commited by vinay

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import AddNewTask from "./component/AddNewTask/addNewTask";
 import Heading from "./component/Title/Heading";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Heading />
-      <AddNewTask />
-    </>
-  );
+  const [userLogin, setLogin] = useState(false);
+  if (userLogin) {
+    return (
+      <>
+        <Heading />
+        <AddNewTask />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Login First</h1>
+      </>
+    );
+  }
 }
 
 export default App;

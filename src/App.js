e9c5699c15d992a7ea2c28c95cc70aecebb351aa
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-03-10 00:46:45 +0530

    commmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Heading from "./component/Title/Heading";
 
 function App() {
   return (
     <>
-      <h1>App </h1>
+      <Heading />
     </>
   );
 }
---
+++ src/component/Title/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <>
+      <h1>App </h1>
+    </>
+  );
+};
+
+export default Heading;

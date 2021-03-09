3c0f91e1b75e6ffde6dfee67b4ae220e12f6a294
asharma991 <asharma991@outlook.com>
2021-03-10 01:15:58 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-
+import AddNewTask from "./component/AddNewTask/addNewTask";
 import Heading from "./component/Title/Heading";
 
 function App() {
   return (
     <>
       <Heading />
+      <AddNewTask />
     </>
   );
 }
